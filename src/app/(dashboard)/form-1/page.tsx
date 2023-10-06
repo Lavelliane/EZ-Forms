89eac80121ad60eed9d1c2beeaa40cd3f3cf61ef
Lavelliane <jhurylastre432@gmail.com>
2023-10-06 20:09:40 +0800

    setup react query

--- src/app/(dashboard)/form-1/page.tsx
+++ src/app/(dashboard)/form-1/page.tsx
@@ -1,10 +1,33 @@
-import Header from "../../../../components/ActivityFormOne/Header"
+"use client";
 
-function DashboardPage() {
+import { useMutation } from "@tanstack/react-query";
+import Header from "../../../../components/ActivityFormOne/Header";
+import getFormOneFields from "@/mutations/getFormOneFields";
+import { useEffect, useState } from "react";
+
+function FormOnePage() {
+  const [data, setData] = useState<any>();
+  const { mutate: generateFormOne } = useMutation({
+    mutationFn: getFormOneFields,
+    onSuccess: (data) => {
+      setData(data)
+    }
+  });
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <div className="flex justify-center items-center ">
-        <Header />
+      <button onClick={async () => {
+         await generateFormOne({
+           eventName: "AWS Jupyter Notebooks",
+           orgName: "Computer Driven Enthusiasts",
+           outputType: "description",
+         })
+      }}>Generate</button>
+      <Header />
     </div>
-  )
+  );
 }
-export default DashboardPage
+export default FormOnePage;
---
+++ src/types/FormOneRequest.ts
@@ -0,0 +1,8 @@
+
+type FormOneRequest = {
+    eventName: string;
+    orgName: string;
+    outputType: "description" | "objective";
+}
+
+export default FormOneRequest
