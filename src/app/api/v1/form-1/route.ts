aea75e8176ca93f445e324fd4a627887a29849e3
Lavelliane <jhurylastre432@gmail.com>
2023-10-07 22:25:52 +0800

    changed prompt for description

--- src/app/api/v1/form-1/route.ts
+++ src/app/api/v1/form-1/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request){
                 },
                 {
                     role: "user",
-                    content: `Please provide a detailed and cohesive declarative ${outputType} about the event: ${eventName}. Minimum of 150 characters, Maximum of 300 characters only and avoid imperative statements.`
+                    content: `What is ${eventName} all about? Please provide a detailed and cohesive declarative ${outputType}. Minimum of 150 characters, Maximum of 300 characters only and avoid imperative statements. Remove imperative statements like \'Join us..\'`
                 },
             ]
         })
