d8bc0ad35f58b01303d7d37387fa1c2c9c4b461e
Vince Abella <vinceabella05@gmail.com>
2023-10-10 14:37:56 +0800

    feature/added table component

---
+++ src/app/(dashboard)/forms/form-3/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Header from '../../../../../components/ActivityFormTwo/Header';
+
+function FormTwoPage() {
+	return (
+		<div className='flex justify-center items-center '>
+			<div className='flex justify-start items-start w-full'>
+				<Header />
+			</div>
+		</div>
+	);
+}
+export default FormTwoPage;
