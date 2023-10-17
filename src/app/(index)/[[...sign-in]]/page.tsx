4096d2597077afb3ff399b0f309353b23459f86a
Lavelliane <jhurylastre432@gmail.com>
2023-10-18 00:19:46 +0800

    added redirect when logged in

--- src/app/(index)/[[...sign-in]]/page.tsx
+++ src/app/(index)/[[...sign-in]]/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+
 import { SignIn } from '@clerk/nextjs';
+import { useAuth } from "@clerk/clerk-react";
+import { useRouter } from 'next/navigation'
 
 export default function Page() {
+	const { isLoaded, userId } = useAuth()
+	const router = useRouter()
+
+	if(isLoaded && userId){
+		router.push('/forms/form-1')
+		return
+	}
 	return (
 		<div className='flex items-center justify-center h-screen'>
 			<SignIn />
