ead704836e66a67e2e223d28de470b6e370ba689
Lavelliane <jhurylastre432@gmail.com>
2023-10-18 00:35:19 +0800

    added redirect button

--- src/app/(index)/[[...sign-in]]/page.tsx
+++ src/app/(index)/[[...sign-in]]/page.tsx
@@ -1,20 +1,23 @@
 'use client'
 
 import { SignIn } from '@clerk/nextjs';
-import { useAuth } from "@clerk/clerk-react";
-import { useRouter } from 'next/navigation'
+import { useAuth } from "@clerk/clerk-react";;
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Page() {
 	const { isLoaded, userId } = useAuth()
-	const router = useRouter()
 
-	if(isLoaded && userId){
-		router.push('/forms/form-1')
-		return
-	}
 	return (
 		<div className='flex items-center justify-center h-screen'>
-			<SignIn />
+			{isLoaded && !userId && (<SignIn />)}
+			{isLoaded && userId && (
+				<Link href={"/forms/form-1"}>
+					<Button>
+						EZ Forms here we go!
+					</Button>
+				</Link>
+			)}
 		</div>
 	);
 }
