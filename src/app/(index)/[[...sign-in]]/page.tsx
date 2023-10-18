7460180722620452e00545a35c6567feaef0bc66
Vince Abella <vinceabella05@gmail.com>
2023-10-18 12:05:23 +0800

    test

--- src/app/(index)/[[...sign-in]]/page.tsx
+++ src/app/(index)/[[...sign-in]]/page.tsx
@@ -1,8 +1,12 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignIn, SignedIn } from '@clerk/nextjs';
+import LandingPage from '../../../../components/LandingPage';
 
 export default function Page() {
 	return (
 		<div className='flex items-center justify-center h-screen'>
+			<SignedIn>
+				<LandingPage />
+			</SignedIn>
 			<SignIn />
 		</div>
 	);
