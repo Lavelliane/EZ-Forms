be680cd958a6840754a040c6f58b9389e87102df
Johnfil Initan <njohnfil.pr+github@gmail.com>
2023-10-18 10:34:10 +0800

    Synced remove and edit functions with add function.

--- components/ProgramFlow/ProgramFlow.tsx
+++ components/ProgramFlow/ProgramFlow.tsx
@@ -25,6 +25,7 @@ import TabActivitySettings from './TabActivitySettings';
 import SheetToPDF from './SheetToPDF';
 import getProgramFlowFields from '@/mutations/getProgramFlowFields';
 import { FlowTable } from './FlowTable';
+import { start } from 'repl';
 
 const ProgramFlow = () => {
 	const [form, setForm] = useState<IProgramFlow>(defaultProgramFlow);
@@ -101,7 +102,13 @@ const ProgramFlow = () => {
 	useEffect(() => {
 		setForm({ ...form, ['time']: startTime + ' - ' + endTime });
 	}, [startTime, endTime]);
-	console.log(form);
+
+
+
+
+	//console.log(form);
+
+
 	return (
 		<Card className='flex flex-grow flex-col bg-white hover:shadow-md transition-shadow w-fit h-fit z-10 py-4 sm:px-8 px-0'>
 			<CardHeader className='text-center'>
@@ -253,7 +260,7 @@ const ProgramFlow = () => {
 
 							<div className='w-full flex flex-col gap-2 items-center -translate-x-[78px]'>
 								<div className='flex'>
-									{!form.eventName || !form.time ? (
+									{!form.eventName || (startTime === endTime) ? (
 										<p className=' text-red-500 text-xs'>Activity name, start and end times required.</p>
 									) : (
 										''
@@ -261,7 +268,7 @@ const ProgramFlow = () => {
 								</div>
 								<div className='-translate-x-[70px]'>
 									<Button
-										disabled={!form.eventName || !form.modeOfPresentation || !form.time || isLoading}
+										disabled={!form.eventName || !form.modeOfPresentation || (startTime === endTime) || isLoading}
 										type='submit'
 										onClick={async (e) => {
 											setPresentationMode('faceToFace');
@@ -290,6 +297,9 @@ const ProgramFlow = () => {
 					<FlowTable onChange={onChange} form={form} />
 				</div>
 				<Separator className='my-2' />
+
+
+				{/* ---------- Footer Section ---------- */}
 				<div className='flex flex-col items-center justify-evenly w-full my-4 text-justify'>
 					<p className='text-xs'>
 						<span className='font-semibold'>DATA PRIVACY CONSENT:</span> I give my consent to the University of San
